fee0f8bedeadfa4b19521295a012b97025a3fede
suraj gupta <ssurajgupta110@gmail.com>
2022-12-09 02:21:14 +0545

    a

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ if(!isset($_SESSION['UserData']['Username'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snake Game</title>
-	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
 
